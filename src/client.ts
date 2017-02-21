2f15d79f502f4b4f66116e120a613b69af441e73
Min Kim <hyeonmink@gmail.com>
2017-02-21 11:33:35 -0800

    end of lab code

--- src/client.ts
+++ src/client.ts
@@ -13,7 +13,7 @@ client.on('data', function(data) { //when we get data
 });
 
 client.on('close', function() { //when connection closed
-    
+    process.exit(0)
 });
 
 
@@ -22,6 +22,30 @@ var PORT = 3000;
 
 //connect to the server
 client.connect(PORT, HOST, function() {
+
+    console.log('Connected to: ' + HOST + ':' + PORT);
+    
+    io.setPrompt('> ');
+    io.prompt();
+
+    io.on('line', function(line){
+        switch(line.trim()) {
+        case 'exit':
+            client.end();
+            console.log('client disconnected');
+            process.exit(0);
+            break;
+        default:
+            client.write(line);
+            break;
+        }
+        io.prompt();
+    }).on('close', function() {
+        client.end();
+        console.log('client disconnected');
+        process.exit(0);
+    });
+
 
 });
 
